67b9c055ebcea176d2956fe3ee07757ccc006ea7
vinayvishwkarma <vinaycse0114@gmail.com>
2022-03-24 22:43:16 +0200

    ResourceReaderUtilTest added

--- src/test/java/com/exercise/movie/util/ResourceReaderUtilTest.java
+++ src/test/java/com/exercise/movie/util/ResourceReaderUtilTest.java
@@ -1,9 +1,32 @@
 package com.exercise.movie.util;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.exercise.movie.dto.MovieDto;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 public class ResourceReaderUtilTest {
 
+    private static final String MOVIE_JSON_FILEPATH = "classpath:data/movies.json";
+
+    @Value(MOVIE_JSON_FILEPATH)
+    private Resource resourceFile;
+
+    @Test
+    public void testJsonToMovieDto() throws IOException {
+        List<MovieDto> movieDtoList = ResourceReaderUtil.jsonToMovieDto(resourceFile);
+        assertNotNull(movieDtoList);
+        MovieDto expectedMovieDto = movieDtoList.get(0);
+        assertEquals(expectedMovieDto.getName(), "Don");
+        assertEquals(expectedMovieDto.getGenre().toString(), "DRAMA");
+    }
 }
